30b5f4cac0c50498d69e3ca84c8fa952ecc8c2c8
Paweł Kubiak <pawel.kubiak.dev@gmail.com>
2024-01-12 12:23:20 +0100

    feat: init

---
+++ README.md
@@ -0,0 +1,9 @@
+A template for getting started with htmx in Project IDX
+
+<a href="https://idx.google.com/new?template=https://github.com/9kubczas4/htmx-idx">
+  <img
+    alt="Open in Project IDX"
+    src="https://www.gstatic.com/monospace/230815/openinprojectidx.png"
+    width="170"
+  />
+</a>
---
+++ app/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+var tmpl = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>htmx and Go App</title>
+		<script src="https://unpkg.com/htmx.org@1.9.10" integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" crossorigin="anonymous"></script>
+</head>
+<body>
+    <h1>htmx and Go App</h1>
+    <div id="content" hx-get="/api/data" hx-trigger="load">
+        Loading...
+    </div>
+</body>
+</html>
+`))
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, nil)
+	})
+
+	http.HandleFunc("/api/data", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World - loaded via htmx!")
+	})
+
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
